b49d9a7c76a6ca96b390f70bddc3c8681b637079
noirvin <vin.sp97@googlemail.com>
2020-05-17 03:53:22 -0700

    can add pet to list

--- pets/urls.py
+++ pets/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('pet/create/', views.pet_create, name='pet-create-page'),
+    path('appointment/create/', views.appointment_create, name='appointment-create-page'),
+    path('pets/', views.pets, name="pets"),
+    path('one_pet/<int:pet_id>', views.one_pet, name="pet"),
+    path('calendar/', views.calendar, name="calendar")
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Pet, Appointment
@@ -9,3 +10,51 @@ from .forms import PetForm, AppointmentForm
 
 def home(request):
     return render(request, 'home.html')
+
+def pets(request):
+  context = {
+    'pets': Pet.objects.filter()
+  }
+  return render(request, 'pets.html', context)
+
+def one_pet(request, pet_id):
+    context = {
+    'pet': Pet.objects.get(id=pet_id),
+    'appointments' : Appointment.objects.filter(pet=pet_id).order_by('date_of_appointment')
+    }
+    return render(request, 'pet.html', context)
+
+def calendar(request):
+  context = {
+    'appointments': Appointment.objects.order_by('date_of_appointment')
+  }
+  return render(request, 'calendar.html', context)
+
+
+def pet_create(request):
+    if request.method == 'POST':
+        form = PetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/pets')
+    else:
+        form = PetForm()
+    return render(request,
+                  'create_pet.html',
+                  {
+                      'form': form
+                  })
+
+def appointment_create(request):
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/calender')
+    else:
+        form = AppointmentForm()
+    return render(request,
+                  'create_appointment.html',
+                  {
+                      'form': form
+                  })
